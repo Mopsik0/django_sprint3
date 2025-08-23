377f774f69262bbc514f4db361ee790b3ab17fe6
viktoriagorpenko <emilia00000000000@gmail.com>
2025-08-23 23:54:03 +0300

    feat: sprint3 — модели, вьюхи, шаблоны, локализация

---
+++ blogicum/blog/admin.py
@@ -0,0 +1,31 @@
+from django.contrib import admin
+from .models import Category, Location, Post
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'is_published', 'created_at')
+    list_editable = ('is_published',)
+    prepopulated_fields = {'slug': ('title',)}
+    search_fields = ('title', 'slug')
+    list_filter = ('is_published',)
+
+
+@admin.register(Location)
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ('name', 'is_published', 'created_at')
+    list_editable = ('is_published',)
+    search_fields = ('name',)
+    list_filter = ('is_published',)
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        'title', 'author', 'category', 'location',
+        'pub_date', 'is_published', 'created_at'
+    )
+    list_editable = ('is_published',)
+    search_fields = ('title', 'text')
+    list_filter = ('is_published', 'category', 'location', 'author')
+    date_hierarchy = 'pub_date'
